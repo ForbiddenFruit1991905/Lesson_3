c60e4a71127595a9f2733fee39d7d79690fa693e
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-01-13 18:02:17 +0300

    add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* int n = 5;
+
+int i = 1;
+while (i <= n)
+{
+    Console.Write($"{i*i} ");
+    i++;
+}
+
+n = 10;
+i = 1;
+while (i <= n)
+{
+    Console.Write($"{i*i} ");
+    i++;
+}
+
+n = 15;
+i = 1;
+while (i <= n)
+{
+    Console.Write($"{i*i} ");
+    i++;
+}  */
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
